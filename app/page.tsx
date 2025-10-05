ff61199add794f4e94030bba2a0656f3826ef7ed
mateoalvarez3011-maker <mateoalvarez3011@gmail.com>
2025-10-05 10:36:41 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 style={{ fontSize: 24, marginBottom: 8 }}>¡Build OK!</h1>
+      <p>Si ves esto, Next encontró la carpeta <code>app</code>. Seguimos con APIs y cron.</p>
+    </div>
+  );
+}
